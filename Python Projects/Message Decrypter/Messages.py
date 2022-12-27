e37c5f39dad576609d9663906a76ac7a38785549
Sergio Palomino <sergiobox96@gmail.com>
2022-12-27 11:06:20 -0500

    Var Definition

---
+++ Python Projects/Message Decrypter/Messages.py
@@ -0,0 +1,11 @@
+
+#The objective of this mini project is decrypt messages for random numbers.
+
+
+letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+trys=int(input("Enter the amount of letters you want to decrypt: "))
+numbers=int(input("Enter the value of the letter you want to decrypt (0-25): "))
+
+loopnumbers=[]
+loopnumbers.append(numbers)
